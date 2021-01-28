44ba95b2ec842319c77fdc3911eded3ec06252c7
t-andoAORI <taigo-ando929@g.ecc.u-tokyo.ac.jp>
2021-01-28 15:09:56 +0900

    formatting & pdf convert

--- convert/md2tex.sh
+++ convert/md2tex.sh
@@ -1,22 +1,30 @@
 #!/bin/bash -e
-cd md_en
-filelist=$(ls *.md | rev | cut -c 4-| rev)
+#
+# Test environment
+## pandoc
+# pandoc 2.11.3.2
+# Compiled with pandoc-types 1.22, texmath 0.12.1, skylighting 0.10.2,
+# citeproc 0.3.0.3, ipynb 0.1.0.1
+# Copyright (C) 2006-2020 John MacFarlane. Web:  https://pandoc.org
+
+# Specify the directory markdown file existed
+dir=../draft
+
+# Get markdown file names to be compiled
+filelist=($(ls ${dir}/*.md | rev | cut -c 4-| rev))
 echo ${filelist}
-cd ..
 
-for name in ${filelist}
+# Conversion from markdown to LaTeX
+for name in ${filelist[@]}
 do
-	sed -e s/'\\('/'$'/g   md_en/${name}.md >tmp.md
-	sed -e s/'\\)'/'$'/g   tmp.md >tmp2.md
-	pandoc tmp2.md -o tmp2.tex
-  #sed -e s/'\\\['/''/g   tmp2.tex >tmp3.tex
-	#sed -e s/'\\\]'/''/g   tmp3.tex >tmp4.tex
-  #sed -e s/'aligned'/'eqnarray'/g   tmp4.tex >tex_en/${name}.tex
-	sed -e s/'\\\['/'\\begin\{eqnarray\}'/g   tmp2.tex >tmp3.tex
-	sed -e s/'\\\]'/'\\end\{eqnarray\}'/g    tmp3.tex>tex_en/${name}.tex
-# なぜかうまくいかなかった(必須な変換ではないが、数式1行ずつに式番号をふりたいなら取り除くと良い)
-#	sed -e s/''\\begin\{aligned\}'/''/g   tmp4.tex >tmp5.tex
-#	sed -e s/''\\end\{aligned\}'/''/g   tmp4.tex >tex_en/${name}.tex
-done
+    sed -e s/'\\('/'$'/g ${name}.md | \
+	sed -e s/'\\)'/'$'/g | \
+	pandoc -t latex| \
+	sed -e s/'\\\['/'\\begin\{eqnarray\}'/g | \
+	sed -e s/'\\\]'/'\\end\{eqnarray\}'/g | \
+	sed -e s/'\\\$'/'\$'/g | \
+	sed -e s/'\\toprule'/'\\toprule\\relax'/g | \
+	sed -e s/'\\midrule'/'\\midrule\\relax'/g | \
+	sed -e s/'\\begin{longtable}'/'\\setlength\\LTleft\{0pt\}\\setlength\\LTright\{0pt\}\\begin{longtable}'/g>${name}.tex
 
-rm tmp*
+done
